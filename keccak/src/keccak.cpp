0e85f364dcb79e940c94125ce663c608e3f8d8a3
beac0n <beac0n@beac0n-desktop>
2014-07-24 17:27:43 +0200

    finished implementing keccak functionality

--- keccak/src/keccak.cpp
+++ keccak/src/keccak.cpp
@@ -7,7 +7,6 @@
 #include <string.h>
 #include <stdlib.h>
 #include <iostream>
-#include <iomanip>
 
 #ifndef width
 #define width 200
@@ -41,13 +40,14 @@
 #define sizeOfState 25
 #endif
 
-#ifndef rotate
-#define rotate(x, y) (((x) << (y)) | ((x) >> (64 - (y))))
-#endif
 
-#ifndef coordinate
-#define coordinate(x, y) (x+5*y)
-#endif
+int coordinate(int x, int y) {
+	return (x%5)+5*(y%5);
+}
+
+uint64_t rotate(uint64_t x, int y) {
+	return ((x) << (y)) | ((x) >> ((64) - (y)));
+}
 
 uint64_t state[25];
 
@@ -74,6 +74,71 @@ struct paddedInput {
 	size_t length;
 };
 
+void theta() {
+	uint64_t C[5] = {0,0,0,0,0};
+	uint64_t D[5] = {0,0,0,0,0};
+
+	for(int i = 0; i < 5; ++i) {
+		C[i] =	state[coordinate(i, 0)] ^ state[coordinate(i, 1)] ^
+				state[coordinate(i, 2)] ^ state[coordinate(i, 3)] ^
+				state[coordinate(i, 4)];
+	}
+
+	for(int i = 0; i < 5; ++i) {
+		D[i] = C[(i+4)%5] ^ rotate(C[(i+1)%5], 1);
+	}
+
+	for(int i = 0; i < sizeOfState; ++i) {
+		state[i] ^= D[i%5];
+	}
+}
+
+uint64_t* rhoPi() {
+	uint64_t* B = new uint64_t [sizeOfState];
+	memset(B, 0, sizeOfState);
+
+	for(int x = 0; x < 5; ++x) {
+		for(int y = 0; y < 5; ++y) {
+			B[coordinate(y,(2*x+3*y))] = rotate(state[coordinate(x,y)], cyclicShiftOffsets[coordinate(x,y)]);
+		}
+	}
+
+	return B;
+}
+
+void chi(uint64_t* B) {
+	for(int x = 0; x < 5; ++x) {
+		for(int y = 0; y < 5; ++y) {
+			state[coordinate(x,y)] = B[coordinate(x,y)] ^
+					((~B[coordinate(x+1,y)]) & B[coordinate(x+2, y)]);
+		}
+	}
+}
+
+void iota(int round) {
+	state[0] ^= roundConstants[round];
+}
+
+void keccakf() {
+	for(int i = 0; i < rounds; ++i) {
+		theta();
+		uint64_t* B = rhoPi();
+		chi(B);
+		iota(i);
+	}
+}
+
+void absorb(paddedInput paddedInput) {
+	for(size_t i = 0; i < paddedInput.length; i += bitrate) {
+		for(int j = 0; j < blockLength; ++j) {
+			state[j] ^= *((uint64_t*) (paddedInput.paddedInput+i+(j*8)));
+		}
+
+		keccakf();
+
+	}
+}
+
 paddedInput padd(const uint8_t* input, size_t inputLength) {
 	size_t blockCount = inputLength / bitrate;
 	size_t newSizeByte = (blockCount + 1) * bitrate;
@@ -92,18 +157,6 @@ paddedInput padd(const uint8_t* input, size_t inputLength) {
 	return returnStruct;
 }
 
-void keccakf() {
-
-}
-
-void absorb(paddedInput paddedInput) {
-	for(int i = 0; i < paddedInput.length; i += bitrate) {
-		for(int j = 0; j < blockLength; ++j) {
-			state[j] ^= *((uint64_t*) (paddedInput.paddedInput+i+(j*8)));
-		}
-	}
-}
-
 uint8_t* squeez() {
 	uint64_t* Z = new uint64_t [blockLength];
 
@@ -113,6 +166,8 @@ uint8_t* squeez() {
 
 	uint8_t* returnValue = new uint8_t [outputLength];
 	memcpy(returnValue, Z, outputLength);
+
+	delete(Z);
 
 	return returnValue;
 }
@@ -127,17 +182,22 @@ uint8_t* keccak(const uint8_t* input, int inputLength) {
 	//absorbing phase
 	absorb(paddedInput);
 
+	delete(paddedInput.paddedInput);
+
 	// squeesing phase
 	return squeez();
 }
 
 int main() {
-	const char* test = "this is a test";
-	int size = 14;
+	const char* test = "Keccak-256 Test Hash";
+	int size = strlen(test);
 
-	std::cout << rounds << std::endl;
+	uint8_t* retVal = keccak((uint8_t *)test,size);
 
-	keccak((uint8_t *)test,size);
+	size = strlen((char*)retVal);
+	for(int i = 0; i < size; ++i) {
+		std::cout << std::uppercase << std::hex << int(retVal[i]) << " ";
+	}
 
 	return 0;
 }
